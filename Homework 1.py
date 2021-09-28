c17348030e5b3aa53a497389c3f573af33212fd3
vulcan6 <ejimene9@cougarnet.uh.edu>
2021-09-28 12:18:42 -0500

    Created DB connectin

--- Homework 1.py
+++ Homework 1.py
@@ -1,3 +1,49 @@
 # Homework 1
 # Erick Jimenez
 # 1463639
+
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+conn = create_connection("cis3368.cdmtjsnscdxt.us-east-2.rds.amazonaws.com", "admin", "Infernus0!", "cis3368")
+cursor = conn.cursor(dictionary=True)
+sql = "SELECT * FROM users"
+cursor.execute(sql)
+rows = cursor.fetchall()
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor(dictionary=True)
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
